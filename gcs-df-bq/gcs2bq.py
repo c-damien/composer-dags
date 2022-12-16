9f863782a737a75df352cb1dff45f4bf1374f216
cdamien <cdamien@google.com>
2022-12-16 14:13:35 -0800

    code cleansing
    update hardcoded parameters and removed unnecessary comments

--- gcs-df-bq/gcs2bq.py
+++ gcs-df-bq/gcs2bq.py
@@ -23,15 +23,12 @@ import time
 import traceback
 import argparse
 
-# Beam and interactive Beam imports
+# Beam imports
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.io.gcp.pubsub import ReadFromPubSub
 from apache_beam.io.fileio import WriteToFiles
 
-#from apache_beam.runners.interactive.interactive_runner import InteractiveRunner
-#import apache_beam.runners.interactive.interactive_beam as ib
-#from apache_beam.runners import DataflowRunner
 
 from apache_beam import window
 
@@ -39,23 +36,23 @@ def ingest_to_bq(known_args, options):
     
  
     #options = PipelineOptions(
-    #    streaming=True,
     #    project=project,
     #    region=region,
-    #    staging_location="gs://streaming-demo-dpd/tmp/staging",
-    #    temp_location="gs://streaming-demo-dpd/tmp/temp"
+    #    input='gs://demo-settings/composer_sensed_file/in/employees.csv'
+    #    output='dataplatformdaydemo:composer_demo.df_results'
     #)
+
     logging.info("\n Reading from" + known_args.input)
     logging.info("\n Writting to" + known_args.output)
 
 
-    with beam.Pipeline(options=options) as p: #p = beam.Pipeline(DataflowRunner() , options=options) #DataflowRunner() InteractiveRunner()
+    with beam.Pipeline(options=options) as p:
     
       
-      pb_data = (p      | "Read file" >> beam.io.ReadFromText('gs://demo-settings/composer_sensed_file/in/employees.csv')
+      pb_data = (p      | "Read file" >> beam.io.ReadFromText(known_args.input)
                         | 'Split records' >> beam.Map(lambda x: x.split(','))
                         | 'Prepare Data' >> beam.Map(lambda x: {"EMPLOYEE_ID": int(x[0]), "FIRST_NAME": x[1], "LAST_NAME": x[2], "EMAIL": x[3], "PHONE_NUMBER": x[4], "ROLE": x[5], "SALARY": int(x[6])}) 
-                        | 'write to bq' >> beam.io.WriteToBigQuery(table="dataplatformdaydemo:composer_demo.df_results", schema='EMPLOYEE_ID:INT64,FIRST_NAME:string,LAST_NAME:string,EMAIL:string,PHONE_NUMBER:string,ROLE:string,SALARY:INT64',
+                        | 'write to bq' >> beam.io.WriteToBigQuery(table=known_args.output, schema='EMPLOYEE_ID:INT64,FIRST_NAME:string,LAST_NAME:string,EMAIL:string,PHONE_NUMBER:string,ROLE:string,SALARY:INT64',
                         write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND,
                         create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED)    
       )
